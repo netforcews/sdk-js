5d5a36e08784084ffd276662b44d75a3a3dd8b6c
BrunoGoncalves <bugotech@gmail.com>
2020-03-11 17:36:13 -0300

    delete do resource

--- src/SdkResource.js
+++ src/SdkResource.js
@@ -9,29 +9,53 @@ class SdkResource extends SdkClient
         this.$part = part;
     }
 
+    /**
+     * Listar recursos.
+     */
     async list()
     {
         return await this.requestJson('get', this.$part);
     }
 
+    /**
+     * Exibir recurso.
+     */
     async show(id)
     {
         return await this.requestJson('get', this.$part + '/' + id);
     }
 
+    /**
+     * Salvar novo recurso.
+     */
     async store(data)
     {
-        //..
+        return await this.requestJson('post', this.$part, data);
     }
 
+    /**
+     * Atualizar um recurso.
+     */
     async update(id, data)
     {
-        //..
+        return await this.requestJson('put', this.$part + '/' + id, data);
     }
 
+    /**
+     * Excluir um recurso.
+     */
     async delete(id)
     {
-        //..
+        var part = this.$part;
+        var params = {};
+
+        if ((typeof id == 'object') && ('length' in id)) {
+            params.ids = id.join(',');
+        } else {
+            part += '/' + id;
+        }
+
+        return await this.requestJson('delete', part, params);
     }
 }
 
